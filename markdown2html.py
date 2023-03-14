c74e7c297ab8d9acf7436e387715b4ca3fb73c55
Mariga94 <56932629+Mariga94@users.noreply.github.com>
2023-03-14 15:24:36 +0300

    Fix code

--- markdown2html.py
+++ markdown2html.py
@@ -9,7 +9,8 @@ if __name__ == "__main__":
     if size < 3:
         print("Usage: ./markdown2html.py README.md README.html", file=stderr)
         exit(1)
-    my_path = path.exists(argv[1])
-    if my_path is False:
+    markdown_file = argv[1]
+    output_file = argv[2]
+    if not (path.exists(markdown_file) and path.islink(markdown_file)):
         print("Missing {}".format(argv[1]), file=stderr)
         exit(1)
